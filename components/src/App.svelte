9ebf38cbcf3d11eaf124c37c4b3608547a5abe78
johan.kivi <johan.kivi@zocom.se>
2022-03-14 15:41:35 +0100

    html added

---
+++ components/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+
+
+</script>
+
+<main class="app">
+	<header>
+		<h1>Super Awesome Todo App 2.0</h1>
+		<p>Du har 0 todos att göra</p>
+	</header>
+	<section>
+		<input type="text">
+		<button>Add todo!</button>
+	</section>
+	<ul class="todos">
+		<li><input type="checkbox"  /> Do this!</li>
+		<li><input type="checkbox"  /> Also do that!</li>
+	</ul>
+</main>
+
+<style>
+
+</style>
