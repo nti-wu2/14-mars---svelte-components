cea7d98db8952bb6169e194921fd2aff2beef5ed
johan.kivi <johan.kivi@zocom.se>
2022-03-14 16:01:09 +0100

    todo one component done

--- components/src/App.svelte
+++ components/src/App.svelte
@@ -1,20 +1,35 @@
 <script>
+const title = 'Super Awesome Todo App 2.0';
 
+let todos = [
+	{ text: 'Köp äpplen', done: false },
+	{ text: 'Köp bananer', done: false }
+];
+
+let newTodo = '';
+
+function addTodo(){
+	todos = [...todos, { text: newTodo, done: false }];
+	newTodo = '';
+}
 
 </script>
 
 <main class="app">
 	<header>
-		<h1>Super Awesome Todo App 2.0</h1>
-		<p>Du har 0 todos att göra</p>
+		<h1>{title}</h1>
+		<p>Du har <b>{todos.length}</b> todos kvar att göra.</p>
 	</header>
 	<section>
-		<input type="text">
-		<button>Add todo!</button>
+		<input type="text" bind:value={newTodo}>
+		<button on:click={addTodo}>Add todo!</button>
 	</section>
 	<ul class="todos">
-		<li><input type="checkbox"  /> Do this!</li>
-		<li><input type="checkbox"  /> Also do that!</li>
+		{#each todos as todo}
+		<li class:done={todo.done}>
+			<input type="checkbox" bind:checked={todo.done}>{todo.text}
+		</li>
+		{/each}
 	</ul>
 </main>
 
